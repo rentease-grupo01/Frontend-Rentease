97251eee3392396179197e2df7110b93874f6176
Oscar <zeoz1012@gmail.com>
2024-06-30 12:30:48 -0500

    service agregado/ faltan estilos

--- src/app/Servicios/payment-history.service.ts
+++ src/app/Servicios/payment-history.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PaymentHistoryService {
+  private apiUrl = 'http://localhost:8081/payments'; // Asegúrate de que la URL sea correcta
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getPaymentHistoryByUserId(userId: number): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/user/${userId}`);
+  }
 }
