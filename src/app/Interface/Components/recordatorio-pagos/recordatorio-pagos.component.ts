9c8a6b04110ddbb226eed4db88bbe2c85b0d374d
Oscar <zeoz1012@gmail.com>
2024-06-30 12:48:52 -0500

    .

--- src/app/Interface/Components/recordatorio-pagos/recordatorio-pagos.component.ts
+++ src/app/Interface/Components/recordatorio-pagos/recordatorio-pagos.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PaymentRemindersService } from '../../../Servicios/recordatorio.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-recordatorio-pagos',
   templateUrl: './recordatorio-pagos.component.html',
   styleUrls: ['./recordatorio-pagos.component.css']
 })
-export class RecordatorioPagosComponent {
+export class PaymentRemindersComponent implements OnInit {
+  paymentReminders: any[] = [];
 
+  constructor(
+    private paymentRemindersService: PaymentRemindersService,
+    private snackBar: MatSnackBar
+  ) { }
+
+  ngOnInit(): void {
+    const userId = 1; // Aquí deberías obtener el ID del usuario de manera dinámica
+    this.paymentRemindersService.getNotificationsByUserId(userId).subscribe(data => {
+      this.paymentReminders = data;
+
+      // Mostrar notificaciones
+      this.showNotifications();
+    });
+  }
+
+  showNotifications() {
+    this.paymentReminders.forEach(notification => {
+      this.openSnackBar(notification.message, 'Marcar como leído', notification.id);
+    });
+  }
+
+  openSnackBar(message: string, action: string, notificationId: number) {
+    let snackBarRef = this.snackBar.open(message, action, {
+      duration: 5000, // Duración en milisegundos
+      horizontalPosition: 'center',
+      verticalPosition: 'bottom'
+    });
+
+    snackBarRef.onAction().subscribe(() => {
+      this.paymentRemindersService.markNotificationAsRead(notificationId).subscribe(() => {
+        // Eliminar la notificación de la lista local después de marcarla como leída
+        this.paymentReminders = this.paymentReminders.filter(n => n.id !== notificationId);
+      });
+    });
+  }
 }
